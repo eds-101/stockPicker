01f15622de7e7690c5b932152ab3499efc5e818b
Edeman George <71378676+eds-101@users.noreply.github.com>
2021-12-24 21:53:59 +0000

    tesla api call

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect } from 'react';
+
+const stocksUrl = 'https://query1.finance.yahoo.com/v7/finance/quote?lang=en-US&region=US&corsDomain=finance.yahoo.com&symbols=TSLA'
+
+async function getStocks() {
+  const response = await fetch(stocksUrl)
+  return response.json()
+}
 
 function App() {
+  
+  useEffect(() => {
+    getStocks()
+      .then((data) => {
+        console.log(data)
+      })
+  }, [])
+
   return (
     <div className="App">
       <header className="App-header">
