3884185c72efda7638ce211fecbc6ec4833ecd0c
Edeman George <71378676+eds-101@users.noreply.github.com>
2022-01-11 00:21:47 +0000

    updating stock price

--- client/src/App.js
+++ client/src/App.js
@@ -3,29 +3,37 @@ import { useEffect, useState } from 'react';
 
 const randomNumber = "http://www.randomnumberapi.com/api/v1.0/random?min=100&max=1000&count=1"
 
+async function fetchStockPrice() {
+  try {
+    const res = await fetch(randomNumber)
+    const result = await res.json()
+    console.log(result[0])
+    return result[0]
+  } catch (e) {
+    console.error(e)
+  }
+}
+
 function App() {
   const [stockPrice, setStockPrice] = useState(1) 
 
-
-  useEffect( () => {
-    async function fetchStockPrice() {
-      try {
-        const res = await fetch(randomNumber)
-        const result = await res.json()
-        console.log(result[0])
-        return result[0]
-      } catch (e) {
-        console.error(e)
-      }
-    }
-    const latestStockPrice = fetchStockPrice()
+useEffect( () => {
+  const timeoutId = setInterval(async () => {
+    console.log('initial price ', stockPrice)
+    const latestStockPrice = await fetchStockPrice()
     setStockPrice(latestStockPrice)
 
-  }, [])
-    
+  }, 5000)
 
+  return () => clearInterval(timeoutId)
+
+
+
+}, [])
+    
   return (
     <div>
+      {'$' + stockPrice}
     </div>
   );
 }
