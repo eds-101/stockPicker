aa9e057df10f98af6652608cacb0acbbdf2f261c
Edeman George <71378676+eds-101@users.noreply.github.com>
2022-01-11 00:05:12 +0000

    set price to number from api

--- client/src/App.js
+++ client/src/App.js
@@ -4,24 +4,28 @@ import { useEffect, useState } from 'react';
 const randomNumber = "http://www.randomnumberapi.com/api/v1.0/random?min=100&max=1000&count=1"
 
 function App() {
-  
+  const [stockPrice, setStockPrice] = useState(1) 
+
+
   useEffect( () => {
-    async function fetchData() {
+    async function fetchStockPrice() {
       try {
         const res = await fetch(randomNumber)
         const result = await res.json()
-        console.log(result)
+        console.log(result[0])
+        return result[0]
       } catch (e) {
         console.error(e)
       }
     }
-    fetchData()
+    const latestStockPrice = fetchStockPrice()
+    setStockPrice(latestStockPrice)
+
   }, [])
     
 
   return (
     <div>
-
     </div>
   );
 }
