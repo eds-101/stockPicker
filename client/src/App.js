c3aef854d3f7506cb08eaa721c6eea6d32b3e0db
Edeman George <71378676+eds-101@users.noreply.github.com>
2022-01-12 20:34:19 +0000

    colour feature working for ticker

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,17 @@
 import './App.css';
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useReducer } from 'react';
 
 const randomNumber = "http://www.randomnumberapi.com/api/v1.0/random?min=100&max=1000&count=1"
 
 async function fetchStockPrice() {
+  // Needs CORS approval when running on localhost
+  // Used Moesif CORS Google Chrome extension to bypass this
   try {
     const res = await fetch(randomNumber)
     const result = await res.json()
     console.log(result[0])
     return result[0]
-  } catch (e) {
+  } catch(e) {
     console.error(e)
   }
 }
@@ -20,24 +22,57 @@ function App() {
     previous: 0,
     history: []
   })
+  const [priceTime, setPriceTime] = useState(null)
+  // const [priceColour, setPriceColour] = useReducer(updateColour, '');
   
-useEffect( () => {
-  const timeoutId = setInterval(async () => {
-    const latestStockPrice = await fetchStockPrice()
-    setStockPrice(oldPrices => { return {
-      latest: latestStockPrice, 
-      previous: oldPrices.latest, 
-      history: [...oldPrices.history, latestStockPrice]} })
-  }, 5000)
+  // function updateColour(state, action) {
+  //   switch (action.type) {
+  //     case(stockPrice.previous < stockPrice.latest):
+  //       return 'greenText'
+  //     case(stockPrice.previous > stockPrice.latest):
+  //       return 'redText'
+  //     default:
+  //       return ""
+  //   }
+  // }
 
-  return () => clearInterval(timeoutId)
-}, [])
+  useEffect(() => {
+    let timeoutId;
+    async function updateTime() {
+      setPriceTime(new Date().toString())
+      timeoutId = setTimeout(updateTime, 5000)
+    }
+    updateTime()
+    return () => {
+      clearTimeout(timeoutId)
+    }
+  }, )
+
+  useEffect(() => {  
+    async function updateStockPrice() {  
+      const latestStockPrice = await fetchStockPrice()
+      setStockPrice(oldPrices => { 
+        return {
+        latest: latestStockPrice, 
+        previous: oldPrices.latest, 
+        history: [...oldPrices.history, latestStockPrice]
+        }}
+      )
+    }
+    updateStockPrice()
+  }, [priceTime])
     
   return (
     <div>
-      {'Prev price: $' + stockPrice.previous} <br/>
-      {'Current price: $' + stockPrice.latest} <br/>
-      {'History: ' + stockPrice.history} 
+      <div className={['price', 
+                      stockPrice.previous < stockPrice.latest ?'greenText' 
+                      : stockPrice.previous > stockPrice.latest ? 'redText' 
+                      : ''].join(" ")}>
+        {'Prev price: $' + stockPrice.previous} <br/>
+        {'Current price: $' + stockPrice.latest} <br/>
+        {'History: ' + stockPrice.history} <br/>
+        {priceTime && priceTime}
+      </div>
     </div>
   );
 }
---
+++ cuong.js
@@ -0,0 +1,57 @@
+```import './App.css';
+import { useEffect, useState } from 'react';
+import axios from 'axios'
+
+// const stocksUrl = 'https://query1.finance.yahoo.com/v7/finance/quote?lang=en-US&region=US&corsDomain=finance.yahoo.com&symbols=TSLA'
+
+// async function getStocks() {
+//   const response = await fetch(stocksUrl)
+//   return response.json()
+// }
+
+const ranNumURL = "http://www.randomnumberapi.com/api/v1.0/random?min=100&max=1000&count=1"
+
+function App() {
+  const [price, setPrice] = useState(100)
+  const [prevPrice, setPrevPrice] = useState(0)
+  const [priceTime, setPriceTime] = useState(null)
+
+  useEffect(() => {
+    let timeoutId;
+
+    // setPrevPrice(price)
+    async function getLatestPrice() {
+
+      setPriceTime(new Date().toString())
+      timeoutId = setTimeout(getLatestPrice, 2000)
+    }
+    getLatestPrice()
+    return () => {
+      // Stops updating if the component is unmounted by clearing the timer
+      clearTimeout(timeoutId)
+    }
+  }, [])
+
+  useEffect(() => {
+    setPrevPrice(price)
+    axios.get(ranNumURL).then(res => {
+      console.log(res.data[0]);
+      setPrice(res.data[0])
+    }).catch(err => console.log(err))
+  }, [priceTime])
+
+  return (
+
+    <div>
+      <div className={['price', prevPrice < price ? 'up' : prevPrice > price ? 'down' : ''].join(" ")}>
+        {/* <div className={['price', prevPrice < price ? 'up' : 'down'].join(" ")}> */}
+        {prevPrice} ==> {price}
+      </div>
+      <div className="price-time">
+        {priceTime && priceTime}
+      </div>
+    </div>
+  );
+}
+
+export default App;```
