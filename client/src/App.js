137acb75b2fad56dec337d53f1706d5705779597
Edeman George <71378676+eds-101@users.noreply.github.com>
2022-01-18 22:58:12 +0000

    new yahoo api url

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,9 @@
 import './App.css';
 import { useEffect, useState, useReducer } from 'react';
+import Highcharts from 'highcharts/highstock'
+import HighchartsReact from 'highcharts-react-official';
 
-const stocksUrl = 'https://query1.finance.yahoo.com/v7/finance/quote?lang=en-US&region=US&corsDomain=finance.yahoo.com&symbols=TSLA'
+const stocksUrl = 'https://yahoo-finance-api.vercel.app/tsla'
 
 async function fetchStockData() {
   // Needs CORS approval when running on localhost
@@ -13,6 +15,101 @@ async function fetchStockData() {
     console.error(e)
   }
 }
+
+var data = [
+  [1491226200000, 143.71, 144.12, 143.05, 143.7, 19985700],
+  [1491312600000, 143.25, 144.89, 143.17, 144.77, 19891400],
+  [1491399000000, 144.22, 145.46, 143.81, 144.02, 27717900],
+  [1491485400000, 144.29, 144.52, 143.45, 143.66, 21149000],
+  [1491571800000, 143.73, 144.18, 143.27, 143.34, 16672200],
+  [1491831000000, 143.6, 143.88, 142.9, 143.17, 18933400]
+]
+
+var ohlc = [],
+  volume = [],
+  dataLength = data.length,
+  groupingUnits = [
+    [
+      "week", // unit name
+      [1] // allowed multiples
+    ],
+    ["month", [1, 2, 3, 4, 6]]
+  ],
+  i = 0;
+
+for (i; i < dataLength; i += 1) {
+  ohlc.push([
+    data[i][0], // the date
+    data[i][1], // open
+    data[i][2], // high
+    data[i][3], // low
+    data[i][4] // close
+  ]);
+
+  volume.push([
+    data[i][0], // the date
+    data[i][5] // the volume
+  ]);
+}
+const options = {
+  rangeSelector: {
+    selected: 1
+  },
+
+  title: {
+    text: "TSLA Historical"
+  },
+
+  yAxis: [
+    {
+      labels: {
+        align: "right",
+        x: -3
+      },
+      title: {
+        text: "Open Hi Low Close"
+      },
+      height: "60%",
+      lineWidth: 2,
+      resize: {
+        enabled: true
+      }
+    },
+    {
+      labels: {
+        align: "right",
+        x: -3
+      },
+      title: {
+        text: "Volume"
+      },
+      top: "65%",
+      height: "35%",
+      offset: 0,
+      lineWidth: 2
+    }
+  ],
+
+  series: [
+    {
+      type: "candlestick",
+      name: "TESLA",
+      data: ohlc,
+      dataGrouping: {
+        units: groupingUnits
+      }
+    },
+    {
+      type: "column",
+      name: "Volume",
+      data: volume,
+      yAxis: 1,
+      dataGrouping: {
+        units: groupingUnits
+      }
+    }
+  ]
+};
 
 function App() {
   const [stockPrice, setStockPrice] = useState({
@@ -36,7 +133,7 @@ function App() {
     async function updateStockData() {  
       const stockData = await fetchStockData()
       console.log(stockData)
-      const latestStockPrice = stockData.quoteResponse.result[0].regularMarketPrice.toFixed(2)
+      const latestStockPrice = stockData.chart.result[0].meta.regularMarketPrice
       setStockPrice(oldPrices => { 
         return {
         latest: latestStockPrice, 
@@ -56,8 +153,14 @@ function App() {
                       : ''].join(" ")}>
         {'TSLA'} <br/>
         {'Current price: $' + stockPrice.latest} <br/>
-        {/* {'History: ' + stockPrice.history} <br/> */}
         {priceTime}
+      </div>
+      <div>
+      <HighchartsReact
+        highcharts={Highcharts}
+        constructorType={'stockChart'}
+        options={options}
+      />
       </div>
     </div>
   );
